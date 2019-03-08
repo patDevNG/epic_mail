03c45144bfb852e62bbe93f62fd61e39fd7bdb43
patDev <cgrleadership@gmail.com>
2019-03-08 10:56:00 +0100

    ft (user signup) user is able to sign up to epic mail -the email is unique and throws up error if one tries to sign up with an already used email [delivers-164382469]

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const users = require('./routes/user');
+const user = require('./routes/user');
 
 const app = express();
 
@@ -13,10 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.use('/', users);
-// app.use((req,res,next)=>{
-//     next(createError(404));
-// });
+app.use('/', user);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 
 router.post('/api/v1/auth/signup', UserController.signUp);
-// router.post('/api/v1/auth/login', UserController.login);
+
 
 
 module.exports = router;
