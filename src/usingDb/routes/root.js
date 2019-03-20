b34b3c664e665e4c255e3ca647700e9676cea421
patDevNG <cgrleadership@gmail.com>
2019-03-20 13:39:35 +0100

    ft(create group) this enable user create a group [delivers 164749441]

--- src/usingDb/routes/root.js
+++ src/usingDb/routes/root.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import auth from './auth';
 import message from './message';
+import groups from '../routes/groups';
 const router = express.Router();
 
 
 router.use('/auth',auth);
 router.use('/messages',message);
+router.use('/groups',groups);
 
 export default router;
