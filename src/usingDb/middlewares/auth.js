5a563dfe9abb6d98cb60958398b3473c739a242e
patDevNG <cgrleadership@gmail.com>
2019-03-21 23:28:59 +0100

    bg(authentication middleware) this serves to protect route from unauthorized users [bg-164817698]

--- src/usingDb/middlewares/auth.js
+++ src/usingDb/middlewares/auth.js
@@ -1,33 +1,35 @@
 import jwt from 'jsonwebtoken';
+
+import jwtConfig from '../config';
+
 import db from '../config/db';
 
-const Auth = {
-    /**
-   * Verify Token
-   * @param {object} req 
-   * @param {object} res 
-   * @param {object} next
-   * @returns {object|void} response object 
-   */
-  async verifyToken(req,res,next){
-    const token = req.headers['x-access-token'];
-    if(!token) {
-        return res.status(400).send({ 'message': 'Provide Token' });
-      }
-      try {
-        const decoded = await jwt.verify(token, process.env.SECRET);
-        const text = 'SELECT * FROM users WHERE email = $1';
-        const { rows } = await db.query(text, [decoded.email]);
-        if(!rows[0]) {
-          return res.status(400).send({ 'message': 'The token you provided is invalid' });
-        }
-        req.user = { email: decoded.email};
-        next();
-      } catch(error) {
-        return res.status(400).send(error);
-      }
+/*
+*this ensures that routes are guarded with a middleware
+*the token is parsed and decoded each time
+*/
+
+class Auth {
+  static verifyToken(req, res, next) {
+    const token = req.headers['x-access-token']; 
+    if (!token) { 
+      res.status(400).json({ status: 400, error: 'Missing token', success: false });
+    } else { 
+      jwt.verify(token, jwtConfig.secret, (err, result) => { 
+        if (err) return res.status(400).json({ status: 400, error: 'Incorrect credentials', success: false });
+       
+        const queryText = `SELECT * FROM users WHERE id = $1;`;
+        const value = [result.userId];
+
+        db.query(queryText, value) 
+          .then((response) => { 
+            req.user = { id: result.userId };
+            next();
+          }, (error) => {
+            res.status(400).json({ status: 400, error, success: false });
+          });
+      });
     }
   }
-  
-  
-  export default Auth;
+}
+export default Auth;
