f18999f42ed5989b78e85863da3d98dec2c9aab4
patDevNG <cgrleadership@gmail.com>
2019-03-22 06:38:57 +0100

    (bg-(fixed) login authntication [ch-164821647]

--- src/usingDb/controllers/login.js
+++ src/usingDb/controllers/login.js
@@ -10,25 +10,32 @@ import user from '../../usingObjects/data/user';
 export default class LoginController {
     static async login(req, res) {
         try {
-            const userData = {};
-            userData.email = req.body.email;
-            userData.password = req.body.password;
-            let {rowCount,rows} = await db.query(queries.checkIfUserExist,[userData.email]);
-            
-            if(rowCount===0){
-                return res.status(404).json({'status':404,'message':'Email Does not Exist'})
-            }else{
-               const validPassword = await bcrypt.compareSync(userData.password, rows[0].password)
-               if(!validPassword){
-                return res.status(401).json({'status':200,'message':'Invalid Password'})
-               }else{
-                const {id}= rows[0]
-                const token = jwt.sign({ id }, secret.secret, { expiresIn: '24h' });
-                return res.status(200).json({'status':200,token,'message':'Login Successful'})
-               }
-     
+            const validateUser = schema.loginUser(req.body);
+            if(validateUser.error===null){
+                const userData = {};
+                userData = req.body;
+                userData.email = req.body.email;
+                userData.password = req.body.password;
+                let {rowCount,rows} = await db.query(queries.checkIfUserExist,[userData.email]);
                 
+                if(rowCount===0){
+                    return res.status(404).json({'status':404,'message':'Email Does not Exist'})
+                }else{
+                   const validPassword = await bcrypt.compareSync(userData.password, rows[0].password)
+                   if(!validPassword){
+                    return res.status(401).json({'status':401,'message':'Invalid Password'})
+                   }else{
+                    const {id}= rows[0]
+                    const token = jwt.sign({ id }, secret.secret, { expiresIn: '24h' });
+                    return res.status(200).json({'status':200,token,'message':'Login Successful'})
+                   }
+         
+                    
+                }
+            }else{
+                res.status(401).json({'status':401,'message':'Invalid Input'});
             }
+           
                         
 
             
